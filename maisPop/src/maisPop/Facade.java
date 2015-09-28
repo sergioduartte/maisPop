a6c5e6ee80f2a08d44b86e3e8443ebf022a97cf1
Sergio Duarte <sduartecc@gmail.com>
2015-09-28 03:50:03 -0300

    Casos de teste adicionados, erro no import do EasyAccept corrigido.

---
+++ maisPop/src/maisPop/Facade.java
@@ -0,0 +1,13 @@
+package maisPop;
+
+import easyaccept.EasyAccept; 
+
+public class Facade {
+	public static void main(String[] args) {
+	    args = new String[] {"maisPop.Facade", 
+	    					"testes/scripts_de_teste/usecase_1.txt"
+	    					};
+	    EasyAccept.main(args);
+	}
+
+}
